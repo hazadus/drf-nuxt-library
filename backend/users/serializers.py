317c9df12f0f3dbbe5c691b5d08dffc4d259197f
hazadus <hazadus7@gmail.com>
2023-05-21 18:13:27 +0300

    feat(backend): added thumbnails to `Author`, `Book` and `CustomUser` models using `django-imagekit`.

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -3,7 +3,32 @@ from rest_framework import serializers
 from .models import CustomUser
 
 
-class CustomUserDetailSerializer(serializers.ModelSerializer):
+class CustomUserURLRepresentationMixin(serializers.ModelSerializer):
+    """
+    Deliver consistent relative URLs of user profile images.
+    """
+
+    def to_representation(self, instance):
+        """
+        Deliver consistent relative URLs of user profile images.
+        Issue: https://forum.djangoproject.com/t/drf-imagefield-serializes-entire-url-with-domain-name/6975
+        """
+        ret = super().to_representation(instance)
+        ret["profile_image"] = (
+            instance.profile_image.url if instance.profile_image else ""
+        )
+        ret["profile_image_thumbnail_small"] = (
+            instance.profile_image_thumbnail_small.url if instance.profile_image else ""
+        )
+        ret["profile_image_thumbnail_large"] = (
+            instance.profile_image_thumbnail_large.url if instance.profile_image else ""
+        )
+        return ret
+
+
+class CustomUserDetailSerializer(
+    CustomUserURLRepresentationMixin, serializers.ModelSerializer
+):
     """
     Detailed Serializer for CustomUser model.
     """
@@ -17,6 +42,8 @@ class CustomUserDetailSerializer(serializers.ModelSerializer):
             "first_name",
             "last_name",
             "profile_image",
+            "profile_image_thumbnail_small",
+            "profile_image_thumbnail_large",
             "is_active",
             "is_staff",
             "is_superuser",
@@ -25,7 +52,9 @@ class CustomUserDetailSerializer(serializers.ModelSerializer):
         ]
 
 
-class CustomUserMinimalSerializer(serializers.ModelSerializer):
+class CustomUserMinimalSerializer(
+    CustomUserURLRepresentationMixin, serializers.ModelSerializer
+):
     """
     Serializer for CustomUser model - minimal public data.
     """
@@ -36,4 +65,6 @@ class CustomUserMinimalSerializer(serializers.ModelSerializer):
             "id",
             "username",
             "profile_image",
+            "profile_image_thumbnail_small",
+            "profile_image_thumbnail_large",
         ]
