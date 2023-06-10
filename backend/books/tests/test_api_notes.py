ee7c13f27bcf10ba341fbc5b064b30765f36045c
hazadus <hazadus7@gmail.com>
2023-06-10 20:41:50 +0300

    tests(backend): added test for `NoteListView`.

---
+++ backend/books/tests/test_api_notes.py
@@ -0,0 +1,28 @@
+#
+# Tests for `notes/` endpoint.
+# Tests tagged "noci" are excluded when running tests in GutHub Actions (because of missing media files).
+#
+import json
+
+from rest_framework import status
+
+from .base_api_test_case import BaseAPITest
+
+
+class NotesAPITest(BaseAPITest):
+    """
+    Test `notes/` DRF API endpoint.
+    """
+
+    def test_notes_list_without_auth_api(self):
+        """
+        Ensure that `NoteListView`:
+
+        - return `HTTP_401_UNAUTHORIZED` without authorization.
+        """
+        url = "/api/v1/notes/"
+        response = self.client.get(
+            url,
+        )
+        authors = json.loads(response.content)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
