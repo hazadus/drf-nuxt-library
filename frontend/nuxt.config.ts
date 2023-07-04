319a12f6c4e55ec6c8649b4b696f589281d0ad0a
hazadus <hazadus7@gmail.com>
2023-07-04 20:06:48 +0300

    dev(frontend): added Umami tracking code (stats.hazadus.ru).

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -7,4 +7,15 @@ export default defineNuxtConfig({
   },
   modules: ["@vueuse/nuxt", "nuxt-icon", "@pinia/nuxt", "@nuxt/content"],
   ssr: false,
+  app: {
+    head: {
+      script: [
+        {
+          src: "https://stats.hazadus.ru/script.js",
+          async: true,
+          "data-website-id": "958843fa-7e40-42cf-b75e-043dc2ae4480",
+        },
+      ],
+    },
+  },
 });
