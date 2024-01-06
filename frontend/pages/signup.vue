6bd25e5e65dea46b5b84499270f91fa2cab92a88
hazadus <hazadus7@gmail.com>
2024-01-06 16:56:51 +0300

    content(frontend): Updated info on Sign up and About pages.

--- frontend/pages/signup.vue
+++ frontend/pages/signup.vue
@@ -1,32 +1,24 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <Title>
-    Регистрация | Библиотека
-  </Title>
+  <Title> Регистрация | Библиотека </Title>
 
   <!-- Breadcrumbs -->
   <nav class="breadcrumb is-small has-arrow-separator" aria-label="breadcrumbs">
     <ul>
       <li>
-        <NuxtLink to="/">
-          Главная
-        </NuxtLink>
+        <NuxtLink to="/"> Главная </NuxtLink>
       </li>
       <li class="is-active">
-        <NuxtLink to="/signup/">
-          Регистрация
-        </NuxtLink>
+        <NuxtLink to="/signup/"> Регистрация </NuxtLink>
       </li>
     </ul>
   </nav>
 
-  <h2 class="header is-size-2 mb-5">
-    Регистрация
-  </h2>
+  <h2 class="header is-size-2 mb-5">Регистрация</h2>
 
   <BulmaNotification type="warning">
-    Возможность регистрации пока что не реализована! Следите за обновлениями.
+    В настоящий момент работа над проектом приостановлена, поэтому
+    зарегистрироваться нет возможности.
   </BulmaNotification>
 </template>
